676d3960dad1f982c4942778b10664ae06ba7032
Victor Ferman <victor.ferman@gmail.com>
2015-09-23 00:44:58 -0500

    fixed the saml servers, now they use the new information and capabilities of the main server module

--- SamlServer.hs
+++ SamlServer.hs
@@ -12,10 +12,11 @@ import           Server
 import           Types
 
 rpServer :: String -> Server
-rpServer sName = initEmptyServer sName [] ruleMap
+rpServer sName = initEmptyServer sName [] kData [] ruleMap
     where url1 = Url sName "one"
           url2 = Url sName "two"
           url3 = Url sName "three"
+          kData = Map.fromList [("rp", [sName])]
           emptyCSP = Csp { scriptList = [], frameList = [], resourceList = []}
           inst1 = Instruction (Left 1) Rule { rType = RuleType Normal Full,
                     rMethod = Post, rUrl = Left url2,
@@ -47,17 +48,19 @@ rpServer sName = initEmptyServer sName [] ruleMap
                           conditionalList = [] }, fileList = Map.empty }
           ruleMap = Map.fromList [(url1, [([], [], response1)]),
                       (url2, [(["id", "idp"], [], response2)]),
-                      (url3, [(["authAssert"], [], response3)])]
+                      (url3, [(["id", "idp", "authAssert"], [], response3)])]
 
 
 idpServer :: String -> Server
-idpServer sName = initEmptyServer sName [] ruleMap
+idpServer sName = initEmptyServer sName  [] kData []  ruleMap
     where url1 = Url sName "one"
           url2 = Url sName "two"
+          kData = Map.fromList [("id", ["userid"]), ("user", ["uname"]),
+                ("pass", ["pass"]), ("idp",[show url1])]
           emptyCSP = Csp { scriptList = [], frameList = [], resourceList = []}
           inst1 = Instruction (Left 1) Rule { rType = RuleType Normal Full,
                     rMethod = Post, rUrl = Left url2,
-                    rContents = Map.fromList [("user", "?"), ("pass", "?"), ("return", ""), ("idp", "")] }
+                    rContents = Map.fromList [("user", "?"), ("pass", "?"), ("return", ""), ("id", ""), ("idp", "")] }
           component1 = Component { cOrigin = url1, cList = [inst1], cPos = 1,
                          cVisible = True}
           response1 = Response {destinationIdentifier = "", origin = url1,
@@ -67,7 +70,8 @@ idpServer sName = initEmptyServer sName [] ruleMap
                           conditionalList = [] }, fileList = Map.empty }
           inst2 = Instruction (Right True) Rule {rType = RuleType Normal Full,
                     rMethod = Post, rUrl = Right "return",
-                    rContents = Map.fromList [("authAssert","sig id idp")] }
+                    rContents = Map.fromList [("id", ""), ("idp", ""),
+                                  ("authAssert","Sig idp Pri \"idp\"")] }
           response2 = Response { destinationIdentifier = "", origin= url2,
                         resNonce ="", csp = emptyCSP, componentList = [],
                         instructionList = PageInstructions { autoList = [inst2],
@@ -81,5 +85,8 @@ getServers = ([idpS, rpS, rp2S], req)
     where idpS= idpServer "idp"
           rpS = rpServer "rp"
           rp2S = rpServer "rp2"
-          knowledge = Map.fromList [("authAssert", "sig idp idp pk_idp")]
+          pKey = privateKey idpS
+          idpUrl = Url "idp" "one"
+          knowledge = Map.fromList [("authAssert", "Sig idp "++ show idpUrl ++
+                        " "++ show pKey)]
           req = Request "" (Url "rp2" "three") "" Post knowledge
