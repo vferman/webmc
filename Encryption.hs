b2464bfb467b7823bb9a0904af8bf1fa310d1c52
Victor Ferman <victor.ferman@gmail.com>
2016-01-05 12:15:06 -0600

    We can now make simple hashes

--- Encryption.hs
+++ Encryption.hs
@@ -2,6 +2,7 @@ module Encryption
 where
 
 import Types
+import qualified Data.Map as Map
 
 inv:: Key -> Key
 inv (Pub str) = Pri str
@@ -25,3 +26,15 @@ decrypt enc dKey
 verify :: Enc a -> Key -> Maybe a
 verify val@(Sig _ _) key = decrypt val key
 verify (Enc _ _) _ = Nothing
+
+hash :: String -> Known -> Maybe String
+hash cStr known
+    | not (null wList) && head wList == "hash" =
+        let filt = Map.filterWithKey (\k _ -> k `elem` drop 1 wList) known
+            res = Map.foldlWithKey (\acc key val -> acc ++ key ++ " " ++ val ++
+                                        " " ) "" filt
+        in if length (Map.keys filt) == length (drop 1 wList)
+             then Nothing
+             else Just res
+    | otherwise = Nothing
+    where wList = words cStr
